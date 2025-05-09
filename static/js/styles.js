e21127592a7ee44093bfcdc3ae3481cdc42ade1c
Nisha Singh <nisha311035@gmail.com>
2025-05-09 12:03:28 +0530

    Internal Web v1

--- static/js/styles.js
+++ static/js/styles.js
@@ -22,6 +22,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+function togglePassword(inputId, icon) {
+  const input = document.getElementById(inputId);
+  if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove('bx-show');
+      icon.classList.add('bx-hide');
+  } else {
+      input.type = "password";
+      icon.classList.remove('bx-hide');
+      icon.classList.add('bx-show');
+  }
+}
+
 // Function to dismiss the alert
 function dismissAlert() {
     let alertBox = document.getElementById('custom-alert');
